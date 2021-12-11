ec8af794e391775c83a8c874d6579939efde0cc4
fai shaikh <sfaisal180@gmail.com>
2021-12-11 21:06:29 +0530

    Added Crud For LeaveType Controller and Datatables and bootstrap cdn  for using font awesome

--- LeaveManagement/Controllers/LeaveTypesController.cs
+++ LeaveManagement/Controllers/LeaveTypesController.cs
@@ -1,5 +1,7 @@
-﻿using LeaveManagement.Contracts;
+﻿using AutoMapper;
+using LeaveManagement.Contracts;
 using LeaveManagement.Data;
+using LeaveManagement.DataViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,23 +12,38 @@ namespace LeaveManagement.Controllers
 {
     public class LeaveTypesController : Controller
     {
-        private ILeaveTypeRepository _leaveTypeRepository { get; }
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
 
-        public LeaveTypesController(ILeaveTypeRepository leaveTypeRepository)
+        public LeaveTypesController(ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
         {
             _leaveTypeRepository = leaveTypeRepository;
+             _mapper = mapper;
         }
+
+        // NOTE WE WANT TO RETURN VIEWMODELS TO VIEW NOT THE CLASS 
+
+
         [HttpGet]
         public IActionResult Index()
         {
-            var leaveType = _leaveTypeRepository.FindAll();
-            return View(leaveType);
+            var leaveTypes = _leaveTypeRepository.FindAll().ToList();
+            var model = _mapper.Map<List<LeaveType>, List<LeaveTypeViewModel>>(leaveTypes);
+            return View(model);
         }
+
         [HttpGet]
-        public IActionResult Details()
+        public IActionResult Details(int id)
         {
-            var leaveType = _leaveTypeRepository.FindAll();
-            return View(leaveType);
+            var isExist = _leaveTypeRepository.IsExists(id);
+            if (!isExist)
+            {
+                return NotFound();
+            }
+
+            var leaveType = _leaveTypeRepository.FindById(id);
+            var model = _mapper.Map<LeaveTypeViewModel>(leaveType);
+            return View(model);
         }
 
         [HttpGet]
@@ -36,39 +53,91 @@ namespace LeaveManagement.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(LeaveType leaveType)
+        public IActionResult Create(LeaveTypeViewModel leaveTypeViewModel)
         {
-            var data = _leaveTypeRepository.Create(leaveType);
+            if (!ModelState.IsValid)
+            {
+                return View(leaveTypeViewModel);
+            }
+
+            var leaveType = _mapper.Map<LeaveType>(leaveTypeViewModel);
+            leaveType.DateCreated = DateTime.Now;
+            var isSuccess = _leaveTypeRepository.Create(leaveType);
+            if (!isSuccess)
+            {
+                ModelState.AddModelError("", "Something Went Wrong");
+                return View(leaveTypeViewModel);
+            }
             return RedirectToAction("Index");
         }
 
 
         [HttpGet]
-        public IActionResult EditLeaveType()
+        public IActionResult EditLeaveType(int id)
         {
-            return View();
+            if (!_leaveTypeRepository.IsExists(id))
+            {
+                return NotFound();
+            }
+            var leaveType = _leaveTypeRepository.FindById(id);
+            var model = _mapper.Map<LeaveTypeViewModel>(leaveType);
+            return View(model);
         }
 
         [HttpPost]
-        public IActionResult EditLeaveType(LeaveType leaveType)
+        public IActionResult EditLeaveType(LeaveTypeViewModel leaveTypeViewModel)
         {
-            var data = _leaveTypeRepository.Create(leaveType);
+
+            if (!ModelState.IsValid)
+            {
+                return View(leaveTypeViewModel);
+            }
+           var leaveType = _mapper.Map<LeaveType>(leaveTypeViewModel);
+        //   var _leaveType = _leaveTypeRepository.FindById(leaveType.Id);
+            
+            //_leaveType.Name = leaveTypeViewModel.Name;
+            //_leaveType.DefaultDays = leaveTypeViewModel.DefaultDays;
+
+
+            var isSuccess = _leaveTypeRepository.Update(leaveType);
+            if (!isSuccess)
+            {
+                ModelState.AddModelError("", "Something Went Wrong");
+                return View(leaveTypeViewModel);
+            }
             return RedirectToAction("Index");
         }
 
-        [HttpGet]
-        public IActionResult DeleteLeaveType()
+        public ActionResult Delete(int id)
         {
-            return View();
+            if (!_leaveTypeRepository.IsExists(id))
+            {
+                return NotFound();
+            }
+            var leaveType = _leaveTypeRepository.FindById(id);
+            var leaveTypeViewModel = _mapper.Map<LeaveTypeViewModel>(leaveType);
+            return View(leaveTypeViewModel);
         }
 
+        // POST: LeaveTypeController/Delete/5
         [HttpPost]
-        public IActionResult DeleteLeaveType(LeaveType leaveType)
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(LeaveTypeViewModel model)
         {
-            var data = _leaveTypeRepository.Create(leaveType);
-            return RedirectToAction("Index");
+            try
+            {
+                var leaveType = _mapper.Map<LeaveType>(model);
+                var isSucess = _leaveTypeRepository.Delete(leaveType);
+                if (!isSucess)
+                {
+                    return View(model);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
         }
-
-
     }
 }
